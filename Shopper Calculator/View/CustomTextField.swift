52ce0cb6d7d123cc03af8e69a0da948e0b42d4d4
GrayZ27 <i520you.chen@qq.com>
2017-09-24 17:28:01 -0500

    draw a currency symbol inside the textfield

--- Shopper Calculator/View/CustomTextField.swift
+++ Shopper Calculator/View/CustomTextField.swift
@@ -8,15 +8,33 @@
 
 import UIKit
 
-//@IBDesignable
+@IBDesignable
 
 class CustomTextField: UITextField {
 
-    /* Update Builder interface
+
     override func prepareForInterfaceBuilder() {
         customTextField()
     }
-    */
+
+    
+    override func draw(_ rect: CGRect) {
+        
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: frame.size.height / 2 - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.9721407043, green: 0.09617938747, blue: 0.1981451356, alpha: 0.704302226)
+        currencyLbl.textColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+        currencyLbl.layer.cornerRadius = 7
+        currencyLbl.clipsToBounds = true
+        currencyLbl.textAlignment = .center
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -30,11 +48,12 @@ class CustomTextField: UITextField {
         layer.cornerRadius = 7
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2671767979)
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
-            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)])
             attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            textColor = #colorLiteral(red: 0.9675543072, green: 0.9932169693, blue: 1, alpha: 1)
         }
         
     }
